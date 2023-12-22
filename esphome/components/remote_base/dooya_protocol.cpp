9a7990a57636dd1c0c0bb6e9631a5a1864cd0279
Pedro Vanzella <pedro@pedrovanzella.com>
2023-12-22 10:11:39 -0300

    Fix protocol
    Signed-off-by: Pedro Vanzella <pedro@pedrovanzella.com>

--- esphome/components/remote_base/dooya_protocol.cpp
+++ esphome/components/remote_base/dooya_protocol.cpp
@@ -6,19 +6,24 @@ namespace remote_base {
 static const char *const TAG = "remote.dooya";
 
 bool DooyaData::operator==(const DooyaData &other) const {
-  return this->action == other.action && this->address == other.address && this->single_multi == other.single_multi &&
-         this->channel == other.channel && this->key == other.key;
+  return this->address == other.address && this->single == other.single && this->channel == other.channel &&
+         this->action == other.action;
 }
 
 void DooyaProtocol::encode(RemoteTransmitData *dst, const DooyaData &data) {}
 
-optional<DooyaData> DooyaProtocol::decode(RemoteReceiveData data) { return {}; }
+optional<DooyaData> DooyaProtocol::decode(RemoteReceiveData data) {
+  DooyaData result;
+  const auto size = data.size();
+  // here we should return if the size is wrong, but I'm not sure about the size just yet, so I'm just going to log it.
+  ESP_LOGW(TAG, "Received Dooya with size: %d", size);
+
+  return result;
+}
 
 void DooyaProtocol::dump(const DooyaData &data) {
-  ESP_LOGI(TAG,
-           "Received Dooya: action=%" PRIu32 ", address=%" PRIu32 ", single_multi=%" PRIu8 ", channel=%" PRIu8
-           ", key=%" PRIu8,
-           data.action, data.address, data.single_multi, data.channel, data.key);
+  ESP_LOGI(TAG, "Received Dooya: address=%" PRIu32 ", single=%" PRIu8 ", channel=%" PRIu8 ", action=%" PRIu8,
+           data.address, data.single, data.channel, data.action);
 }
 
 }  // namespace remote_base
--- esphome/components/remote_base/dooya_protocol.h
+++ esphome/components/remote_base/dooya_protocol.h
@@ -12,11 +12,10 @@ namespace remote_base {
 struct DooyaData {
   DooyaData() {}
   bool operator==(const DooyaData &other) const;
-  uint32_t action;
   uint32_t address;
-  uint8_t single_multi;
+  bool single;
   uint8_t channel;
-  uint8_t key;
+  uint8_t action;
 };
 
 class DooyaProtocol : public RemoteProtocol<DooyaData> {
@@ -29,14 +28,13 @@ class DooyaProtocol : public RemoteProtocol<DooyaData> {
 DECLARE_REMOTE_PROTOCOL(Dooya)
 
 template<typename... Ts> class DooyaAction : public RemoteTransmitterActionBase<Ts...> {
-  TEMPLATABLE_VALUE(uint32_t, action)
   TEMPLATABLE_VALUE(uint32_t, address)
-  TEMPLATABLE_VALUE(uint8_t, single_multi)
+  TEMPLATABLE_VALUE(bool, single)
   TEMPLATABLE_VALUE(uint8_t, channel)
-  TEMPLATABLE_VALUE(uint8_t, key)
+  TEMPLATABLE_VALUE(uint8_t, action)
   void encode(RemoteTransmitData *dst, Ts... x) {
-    DooyaProtocol().encode(dst, {this->action_.value(x...), this->address.value(x...), this->single_multi_.value(x...),
-                                 this->channel_.value(x...), this->key_.value(x...)});
+    DooyaProtocol().encode(dst, {this->address.value(x...), this->single_.value(x...),
+                                 this->channel_.value(x...), this->action_.value(x...)});
   }
 };
 }  // namespace remote_base
